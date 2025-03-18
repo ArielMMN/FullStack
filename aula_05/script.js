61069f907656a48cb31ba87bddb7ac3de12d2863
ArielMMN <arielmmoraes@gmail.com>
2025-03-17 22:36:41 -0300

    javascript

---
+++ aula_05/script.js
@@ -0,0 +1,30 @@
+//esse e para pop-up = window.alert('olá')
+//console.log('olá');
+
+//let nome = prompt("Qual seu nome?: ")
+
+//console.log(nome)
+
+///let i = 5;
+
+//while (i<=500) {
+//    console.log(i);
+//  i+=5;
+//}
+
+//let num = prompt("digite um numero positivo: ")
+
+//if(num > 0){
+//    console.log("numéro invalido")
+//}
+
+//for (let i = num; i>=0; i--){
+//   console.log(i)
+//}
+
+let num = prompt("digite um numero positivo: ")
+
+if(num > 0){
+    console.log("numéro invalido")
+}else if(num<=0)
+
