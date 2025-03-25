e0b78dde86f56c0b07ef196137316d8473f8f968
ArielMMN <arielmmoraes@gmail.com>
2025-03-24 22:28:12 -0300

    aula 06.2

--- projetc/lab_4/script.js
+++ projetc/lab_4/script.js
@@ -5,9 +5,11 @@ function ex1(){
     let num = parseInt(document.getElementById('num').value);
 
     if(num < n1){
-        document.getElementById('resp').innerHTML = "Maior"
+        document.getElementById('resp').innerHTML = "Maior";
+        document.getElementById('resp').style.setProperty("background-color", "red");
     }else if(num > n1){
-        document.getElementById('resp').innerHTML = "Menor"
+        document.getElementById('resp').innerHTML = "Menor";
+        document.getElementById('resp').style.setProperty("background-color", "red");
     }else if(num === n1){
         document.getElementById('resp').innerHTML = "Acertou";
         document.getElementById('resp').style.setProperty("background-color", "green");
