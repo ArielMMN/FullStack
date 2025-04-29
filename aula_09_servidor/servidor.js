4a28086b4532f23d03801d7f9c67774de59f543e
ArielMMN <arielmmoraes@gmail.com>
2025-04-28 22:39:45 -0300

    aula 10, lab 08

--- aula_09_servidor/servidor.js
+++ aula_09_servidor/servidor.js
@@ -1,8 +1,55 @@
 require("colors")
 var http = require('http');
 var express = require('express');
+var bodyParser = require("body-parser")
 var app = express();
-app.use(express.static('./public'));
+app.use(express.static('public/'));
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
 var server = http.createServer(app);
 server.listen(80);
 console.log("Servidor rodando...".green);
+
+// exemplo de get e post
+
+app.get('/', function(requisicao, resposta){
+    resposta.redirect('projetc/projects.html')
+})
+
+app.get('/cadastra', function(requisicao, resposta){
+    resposta.redirect('projetc/lab_08/Cadastra.html')
+})
+
+app.post('/inicio', function(requisicao, resposta){
+    resposta.redirect('aula_04/index.html')
+})
+
+// query acessa informaçãos atravé da barra
+app.get('/cadastrar', function(requisicao, resposta){
+    let nome = requisicao.query.nome;
+    let email = requisicao.query.email;
+    let senha = requisicao.query.senha;
+    let nascimento = requisicao.query.nascimento;
+    resposta.render('resposta.ejs', 
+        {mensagem: "Cadastro finalizado com sucesso!", usuario: nome, login: email, idade: nascimento })
+
+    console.log(nome,email,senha,nascimento)
+})
+
+//body acessa no corpo da pagina
+app.post('/cadastrar', function(requisicao, resposta){
+    let nome = requisicao.body.nome;
+    let email = requisicao.body.email;
+    let senha = requisicao.body.senha;
+    let nascimento = requisicao.body.nascimento;
+    resposta.render('resposta.ejs', 
+        {mensagem: "Cadastro finalizado com sucesso!", usuario: nome, login: email, idade: nascimento })
+    
+
+    console.log(nome,email,senha,nascimento)
+})
+
+app.get('/for_ejs' , function(requisicao, resposta){
+    let num = requisicao.query.num;
+    resposta.render('exemplo_for.ejs',{tamanho: num});
+})
