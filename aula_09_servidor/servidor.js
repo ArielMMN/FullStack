63118e68daa7e0d1c79744cf2a612c577771969a
ArielMMN <arielmmoraes@gmail.com>
2025-05-05 18:27:11 -0300

    lab 08, finalizado

--- aula_09_servidor/servidor.js
+++ aula_09_servidor/servidor.js
@@ -20,6 +20,10 @@ app.get('/cadastra', function(requisicao, resposta){
     resposta.redirect('projetc/lab_08/Cadastra.html')
 })
 
+app.get('/loga', function(requisicao, resposta){
+    resposta.redirect('projetc/lab_08/Login.html')
+})
+
 app.post('/inicio', function(requisicao, resposta){
     resposta.redirect('aula_04/index.html')
 })
@@ -37,16 +41,26 @@ app.get('/cadastrar', function(requisicao, resposta){
 })
 
 //body acessa no corpo da pagina
-app.post('/cadastrar', function(requisicao, resposta){
+app.post('/cadast', function(requisicao, resposta){
     let nome = requisicao.body.nome;
     let email = requisicao.body.email;
     let senha = requisicao.body.senha;
     let nascimento = requisicao.body.nascimento;
-    resposta.render('resposta.ejs', 
+    resposta.render('respost.ejs', 
         {mensagem: "Cadastro finalizado com sucesso!", usuario: nome, login: email, idade: nascimento })
     
 
     console.log(nome,email,senha,nascimento)
+})
+
+app.post('/log', function(requisicao, resposta){
+    let email = requisicao.body.email;
+    let senha = requisicao.body.senha;
+    resposta.render('respost_log.ejs', 
+        {mensagem: "Login realizado com sucesso!", login: email})
+    
+
+    console.log(email,senha)
 })
 
 app.get('/for_ejs' , function(requisicao, resposta){
